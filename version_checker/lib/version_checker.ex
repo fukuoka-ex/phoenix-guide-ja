975cb2a361afccf32a7dec4acb11c75300b6f224
koga1020 <koga.shozo1020@gmail.com>
2019-11-20 01:09:15 +0900

    init checker project

---
+++ version_checker/lib/version_checker.ex
@@ -0,0 +1,18 @@
+defmodule VersionChecker do
+  @moduledoc """
+  Documentation for VersionChecker.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> VersionChecker.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
