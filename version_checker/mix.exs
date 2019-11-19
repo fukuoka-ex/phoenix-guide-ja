6b39c6c633018303d5413f8e97b1efc3dc8a3328
koga1020 <koga.shozo1020@gmail.com>
2019-11-20 01:15:45 +0900

    add deps

--- version_checker/mix.exs
+++ version_checker/mix.exs
@@ -21,8 +21,8 @@ defmodule VersionChecker.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:git_cli, "~> 0.3.0"},
+      {:tentacat, "~> 1.6.1"}
     ]
   end
 end
